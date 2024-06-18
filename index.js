8d90a2a174287419976c85f646147ae203b7c58a
Marin <kuglermarinkevin@gmail.com>
2024-06-18 08:41:04 -0300

    feat: ordenado array

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let numeros = [4, 3, 1, 4, 3, 5, 7, 7, 8, 9]
+let numeros = [4, 3, 1, 4, 3, 5, 7, 7, 8, 9];
 
+for (let i = 0; i < numeros.length; i++) {
+    console.log("No primeiro laço", numeros[i])
+    for (let j = i + 1; j < numeros.length; j++) {
+        console.log("No segundo laço", numeros[j])
+        if(numeros[i] > numeros[j]) {
+            let aux = numeros[i]
+            numeros[i] = numeros[j]
+            numeros[j] = aux
+        }
+    }
+}
+console.log(numeros)
