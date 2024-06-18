4a6de419b1ef21445adb7d55749b5d46402771a4
Marin <kuglermarinkevin@gmail.com>
2024-06-18 08:52:22 -0300

    feat: otimizando codigo

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ for (let i = 0; i < numeros.length; i++) {
             let aux = numeros[i]
             numeros[i] = numeros[j]
             numeros[j] = aux
+        } else if (numeros[i] == numeros[j]) {
+            numeros.splice(j, 1)
+            j--
         }
+
     }
 }
 console.log(numeros)
 
- for (let i = 0; i < numeros.length; i++) {
-    for (let j = i + 1; j < numeros.length; j++) {
-        if(numeros[i] == numeros[j]) {
-            numeros.splice(j, 1)
-            j--
-        }
-    }
- }
- console.log(numeros)
+//  for (let i = 0; i < numeros.length; i++) {
+//     for (let j = i + 1; j < numeros.length; j++) {
+//         if(numeros[i] == numeros[j]) {
+//             numeros.splice(j, 1)
+//             j--
+//         }
+//     }
+//  }
+//  console.log(numeros)
